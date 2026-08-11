b4383088d24ff1c48d937afdac08bc1be6db9fff
Amanpreet Kaur <105384604+azk001@users.noreply.github.com>
2026-08-11 11:27:19 -0500

    Add files via upload

---
+++ Metagenomics/PartC_Intra_species.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+module load strainest
+source /apps/profiles/modules_asax.sh.dyn
+module load bowtie2/2.5.1
+module load samtools/1.11
+
+strainest mapgenomes SC*.fna Xp91-118.fna MR.fna
+strainest map2snp Xp91-118.fna MR.fna snp.dgrp
+strainest snpdist snp.dgrp snp_dist.txt hist.pdf
+strainest snpclust snp.dgrp snp_dist.txt snp_clust.dgrp clusters.txt
+strainest mapgenomes SC*.fna Xp91-118.fna MA.fna
+bowtie2-build -f MA.fna MA
+
+bowtie2 --very-fast --no-unal -x MA -1 sample_1.fq -2 Sample_2.fq  -S Sample.sam 
+samtools view -b Sample.sam > Sample.bam
+samtools sort Sample.bam -o Sample.sorted.bam
+samtools index Sample.sorted.bam 
+
+strainest est snp_clust.dgrp sample_intra.sorted.bam  ./Sample_Intra
+
